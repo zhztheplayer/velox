debc1d6cabf814ef3806049e44784d08c3ab25c2
Jin Chengcheng <chengcheng.jin@intel.com>
2023-02-13 15:31:03 +0800

    Support parquet read case sensitive mode (#126)

--- velox/connectors/hive/HiveConnector.cpp
+++ velox/connectors/hive/HiveConnector.cpp
@@ -256,6 +256,7 @@ HiveDataSource::HiveDataSource(
     ExpressionEvaluator* expressionEvaluator,
     memory::MemoryAllocator* allocator,
     const std::string& scanId,
+    bool caseSensitive,
     folly::Executor* executor)
     : outputType_(outputType),
       fileHandleFactory_(fileHandleFactory),
@@ -340,6 +341,8 @@ HiveDataSource::HiveDataSource(
     readerOutputType_ = ROW(std::move(names), std::move(types));
   }
 
+  readerOpts_.setCaseSensitive(caseSensitive);
+
   rowReaderOpts_.setScanSpec(scanSpec_);
   rowReaderOpts_.setMetadataFilter(metadataFilter_);
 
@@ -401,7 +404,8 @@ template <TypeKind ToKind>
 velox::variant convertFromString(const std::optional<std::string>& value) {
   if (value.has_value()) {
     // No need for casting if ToKind is VARCHAR or VARBINARY.
-    if constexpr (ToKind == TypeKind::VARCHAR || ToKind == TypeKind::VARBINARY) {
+    if constexpr (
+        ToKind == TypeKind::VARCHAR || ToKind == TypeKind::VARBINARY) {
       return velox::variant(value.value());
     }
     bool nullOutput = false;
@@ -538,10 +542,7 @@ void HiveDataSource::addSplit(std::shared_ptr<ConnectorSplit> split) {
   scanSpec_->resetCachedValues();
 
   // Check filters and see if the whole split can be skipped
-  if (!testFilters(
-          scanSpec_.get(),
-          reader_.get(),
-          split_->filePath)) {
+  if (!testFilters(scanSpec_.get(), reader_.get(), split_->filePath)) {
     emptySplit_ = true;
     ++runtimeStats_.skippedSplits;
     runtimeStats_.skippedSplitBytes += split_->length;
@@ -560,7 +561,8 @@ void HiveDataSource::addSplit(std::shared_ptr<ConnectorSplit> split) {
     static const RowTypePtr kEmpty{ROW({}, {})};
     cs = std::make_shared<dwio::common::ColumnSelector>(kEmpty);
   } else {
-    cs = std::make_shared<dwio::common::ColumnSelector>(fileType, columnNames);
+    cs = std::make_shared<dwio::common::ColumnSelector>(
+        fileType, columnNames, nullptr, readerOpts_.isCaseSensitive());
   }
 
   rowReader_ = reader_->createRowReader(
