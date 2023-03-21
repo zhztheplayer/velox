1430cb9884806317c4ad4984d5d0ad0a7dc1af73
Jin Chengcheng <chengcheng.jin@intel.com>
2023-03-21 10:29:53 +0800

    support make_decimal and null literal for decimal

--- velox/substrait/SubstraitParser.cpp
+++ velox/substrait/SubstraitParser.cpp
@@ -331,7 +331,8 @@ std::string SubstraitParser::mapToVeloxFunction(
   auto it = substraitVeloxFunctionMap_.find(subFunc);
   if (isDecimal) {
     if (subFunc == "add" || subFunc == "subtract" || subFunc == "multiply" ||
-        subFunc == "divide" || subFunc == "avg" || subFunc == "sum") {
+        subFunc == "divide" || subFunc == "avg" || subFunc == "sum" ||
+        subFunc == "round") {
       return "decimal_" + subFunc;
     }
   }
