40ee68c7fc2fa6d01779a527d603bf81adcd5dfb
PHILO-HE <feilong.he@intel.com>
2023-04-12 15:18:55 +0800

    Support substring_index sql function (#189)

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -146,6 +146,9 @@ void registerFunctions(const std::string& prefix) {
   registerFunction<ContainsFunction, bool, Varchar, Varchar>(
       {prefix + "contains"});
 
+  registerFunction<SubstringIndexFunction, Varchar, Varchar, Varchar, int32_t>(
+      {prefix + "substring_index"});
+
   registerFunction<TrimSpaceFunction, Varchar, Varchar>({prefix + "trim"});
   registerFunction<TrimFunction, Varchar, Varchar, Varchar>({prefix + "trim"});
   registerFunction<LTrimSpaceFunction, Varchar, Varchar>({prefix + "ltrim"});
