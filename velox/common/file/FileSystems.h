9b68c024e8641ecce3762aeec5134960d3e9747a
PHILO-HE <feilong.he@intel.com>
2023-04-14 13:09:41 +0800

    Support multiple HDFS endpoints (#174)

--- velox/common/file/FileSystems.h
+++ velox/common/file/FileSystems.h
@@ -89,7 +89,7 @@ std::shared_ptr<FileSystem> getFileSystem(
 // and a lambda that generates the actual file system.
 void registerFileSystem(
     std::function<bool(std::string_view)> schemeMatcher,
-    std::function<std::shared_ptr<FileSystem>(std::shared_ptr<const Config>)>
+    std::function<std::shared_ptr<FileSystem>(std::shared_ptr<const Config>, std::string_view)>
         fileSystemGenerator);
 
 // Register the local filesystem.
