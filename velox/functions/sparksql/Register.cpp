738e964469437d6385b0d6c88b4853146a24990e
Hongze Zhang <hongze.zhang@intel.com>
2023-05-19 22:42:18 +0800

    Implement Spark's version of atan2

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -18,6 +18,7 @@
 #include "velox/functions/lib/IsNull.h"
 #include "velox/functions/lib/Re2Functions.h"
 #include "velox/functions/lib/RegistrationHelpers.h"
+#include "velox/functions/prestosql/Arithmetic.h"
 #include "velox/functions/prestosql/JsonFunctions.h"
 #include "velox/functions/prestosql/Rand.h"
 #include "velox/functions/prestosql/StringFunctions.h"
@@ -249,6 +250,9 @@ void registerFunctions(const std::string& prefix) {
   registerFunction<DateDiffFunction, int32_t, Date, Date>({"date_diff"});
   registerFunction<UnscaledValueFunction, int64_t, UnscaledShortDecimal>(
       {prefix + "unscaled_value"});
+
+  registerFunction<Atan2FunctionIgnoreZeroSign, double, double, double>(
+      {prefix + "atan2"});
 }
 
 } // namespace sparksql
