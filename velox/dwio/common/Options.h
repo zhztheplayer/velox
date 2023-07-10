7f0ca274419567cc7084b57e5b4ea731fb0e1c6d
Rui Mo <rui.mo@intel.com>
2023-07-10 11:16:57 +0800

    Enable nested struct schema pruning (#354)

--- velox/dwio/common/Options.h
+++ velox/dwio/common/Options.h
@@ -111,6 +111,7 @@ class RowReaderOptions {
   std::function<void(
       facebook::velox::dwio::common::flatmap::FlatMapKeySelectionStats)>
       keySelectionCallback_;
+  RowTypePtr outputType_;
 
  public:
   RowReaderOptions() noexcept
@@ -280,6 +281,14 @@ class RowReaderOptions {
    */
   void setAppendRowNumberColumn(bool value) {
     appendRowNumberColumn_ = value;
+  }
+
+  void setOutputType(const RowTypePtr& outputType) {
+    outputType_ = outputType;
+  }
+
+  const RowTypePtr& getOutputType() const {
+    return outputType_;
   }
 
   bool getAppendRowNumberColumn() const {
