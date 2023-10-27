vectorijk/velox-oap-project

Support timestamp reader for Parquet file format (4680)

velox/dwio/parquet/reader/ParquetColumnReader.cpp
@@ -28,6 +28,7 @@
 #include "velox/dwio/parquet/reader/StructColumnReader.h"
 
 #include "velox/dwio/parquet/reader/Statistics.h"
+#include "velox/dwio/parquet/reader/TimestampColumnReader.h"
 #include "velox/dwio/parquet/thrift/ParquetThriftTypes.h"
 
 namespace facebook::velox::parquet {
@@ -75,6 +76,10 @@ std::unique_ptr<dwio::common::SelectiveColumnReader> ParquetColumnReader::build(
     case TypeKind::BOOLEAN:
       return std::make_unique<BooleanColumnReader>(
           requestedType, dataType, params, scanSpec);
+
+    case TypeKind::TIMESTAMP:
+      return std::make_unique<TimestampColumnReader>(
+          dataType, params, scanSpec);
 
     default:
       VELOX_FAIL(

zhztheplayer/velox

velox/dwio/parquet/reader/ParquetColumnReader.cpp
@@ -28,6 +28,7 @@
 #include "velox/dwio/parquet/reader/StructColumnReader.h"
 
 #include "velox/dwio/parquet/reader/Statistics.h"
+#include "velox/dwio/parquet/reader/TimestampColumnReader.h"
 #include "velox/dwio/parquet/thrift/ParquetThriftTypes.h"
 
 namespace facebook::velox::parquet {
@@ -75,6 +76,10 @@ std::unique_ptr<dwio::common::SelectiveColumnReader> ParquetColumnReader::build(
     case TypeKind::BOOLEAN:
       return std::make_unique<BooleanColumnReader>(
           requestedType, fileType, params, scanSpec);
+
+    case TypeKind::TIMESTAMP:
+      return std::make_unique<TimestampColumnReader>(
+          fileType, params, scanSpec);
 
     default:
       VELOX_FAIL(