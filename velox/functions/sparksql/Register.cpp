9843ca0baab459c3b8d31697777dfe7cdd082d59
PHILO-HE <feilong.he@intel.com>
2023-11-27 13:20:55 +0800

    Add simdjson based get_json_object Spark function (5179)

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -42,6 +42,7 @@
 #include "velox/functions/sparksql/RegexFunctions.h"
 #include "velox/functions/sparksql/RegisterArithmetic.h"
 #include "velox/functions/sparksql/RegisterCompare.h"
+#include "velox/functions/sparksql/SIMDJsonFunctions.h"
 #include "velox/functions/sparksql/Size.h"
 #include "velox/functions/sparksql/SparkPartitionId.h"
 #include "velox/functions/sparksql/String.h"
@@ -172,6 +173,9 @@ void registerFunctions(const std::string& prefix) {
   registerSize(prefix + "size");
 
   registerRegexpReplace(prefix);
+
+  registerFunction<SIMDGetJsonObjectFunction, Varchar, Varchar, Varchar>(
+      {prefix + "get_json_object"});
 
   // Register string functions.
   registerFunction<sparksql::ChrFunction, Varchar, int64_t>({prefix + "chr"});
