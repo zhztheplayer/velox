21ca0f2fcd9436ef441eead69e2f4649b0a81510
PHILO-HE <feilong.he@intel.com>
2024-07-03 08:57:08 +0800

    [#5179 ] Optimize get_json_object Spark function using simdjson (5179)
    https://github.com/facebookincubator/velox/pull/5179

--- velox/functions/sparksql/Register.cpp
+++ velox/functions/sparksql/Register.cpp
@@ -35,6 +35,7 @@
 #include "velox/functions/sparksql/DateTimeFunctions.h"
 #include "velox/functions/sparksql/Hash.h"
 #include "velox/functions/sparksql/In.h"
+#include "velox/functions/sparksql/JsonFunctions.h"
 #include "velox/functions/sparksql/JsonObjectKeys.h"
 #include "velox/functions/sparksql/LeastGreatest.h"
 #include "velox/functions/sparksql/MightContain.h"
@@ -178,6 +179,8 @@ void registerFunctions(const std::string& prefix) {
 
   registerFunction<JsonObjectKeysFunction, Array<Varchar>, Varchar>(
       {prefix + "json_object_keys"});
+  registerFunction<GetJsonObjectFunction, Varchar, Varchar, Varchar>(
+      {prefix + "get_json_object"});
 
   // Register string functions.
   registerFunction<sparksql::ChrFunction, Varchar, int64_t>({prefix + "chr"});
