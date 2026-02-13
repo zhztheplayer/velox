181828fc595431b105173bfd1dd344bf16d657a8
Hongze Zhang <hongze.zzz123@gmail.com>
2026-02-13 15:56:18 +0100

    WIP tests, needs verification

--- velox/exec/HashProbe.cpp
+++ velox/exec/HashProbe.cpp
@@ -390,7 +390,7 @@ void HashProbe::pushdownDynamicFilters() {
         }
         auto& hasher = *table_->hashers()[sourceChannel];
         if (hasher.typeKind() == TypeKind::VARCHAR || hasher.typeKind() == TypeKind::VARBINARY) {
-          if (!operatorCtx_->driverCtx()->queryConfig().hashProbeDynamicFilterPushdownEnabled()) {
+          if (!operatorCtx_->driverCtx()->queryConfig().hashProbeStringDynamicFilterPushdownEnabled()) {
             return false;
           }
         }
