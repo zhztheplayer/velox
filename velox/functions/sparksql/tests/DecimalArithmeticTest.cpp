53daafc0381821b4f82d55a1e14259d6be23e195
Hongze Zhang <hongze.zzz123@gmail.com>
2026-09-04 10:32:46 +0200

    fixup

--- velox/functions/sparksql/tests/DecimalArithmeticTest.cpp
+++ velox/functions/sparksql/tests/DecimalArithmeticTest.cpp
@@ -359,13 +359,16 @@ class DecimalArithmeticTest : public SparkFunctionBaseTest {
 TEST_F(DecimalArithmeticTest, fusedIntegralDecimalCast) {
   auto input = makeRowVector(
       {"quantity", "price"},
-      {makeFlatVector<int32_t>({2, 3, 4}),
-       makeFlatVector<int64_t>({300, 250, 125}, DECIMAL(12, 2))});
+      {makeNullableFlatVector<int32_t>({2, std::nullopt, 4, 5, 6}),
+       makeNullableFlatVector<int64_t>(
+           {300, 250, std::nullopt, 125, 100}, DECIMAL(12, 2))});
 
   auto result = evaluate(
       "multiply(cast(quantity as decimal(10, 0)), price)", input);
   assertEqualVectors(
-      makeFlatVector<int128_t>({600, 750, 500}, DECIMAL(23, 2)), result);
+      makeNullableFlatVector<int128_t>(
+          {600, std::nullopt, std::nullopt, 625, 600}, DECIMAL(23, 2)),
+      result);
 }
 
 TEST_F(DecimalArithmeticTest, add) {
